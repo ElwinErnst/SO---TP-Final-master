9a2d3308c1a68797cd444382826af534ffb30e6f
ElwinErnst <80267349+ElwinErnst@users.noreply.github.com>
2021-06-27 09:24:58 -0300

    Add project files.

---
+++ SO - TP Final/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using SO___TP_Final.Controladores;
+
+namespace SO___TP_Final
+{
+    /*
+     BIBLIOGRAFIA
+
+        https://docs.microsoft.com/en-us/dotnet/api/system.threading?view=net-5.0
+        https://www.c-sharpcorner.com/UploadFile/1d42da/thread-locking-in-C-Sharp/
+        https://docs.docker.com/get-started/overview/
+
+     */
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Control.inicializandoCorredores();
+
+            Thread threadTortuga = new Thread(Control.carreraTortuga);
+            threadTortuga.Start();
+
+            Thread threadLiebre = new Thread(Control.carreraLiebre);
+            threadLiebre.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
